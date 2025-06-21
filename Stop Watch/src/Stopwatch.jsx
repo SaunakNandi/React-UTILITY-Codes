542a652edd5c5f150c4dba7a9132bd2c90b669be
SaunakNandi <saunaknandi021000@gmail.com>
2025-06-21 23:59:23 +0530

    Stopwatch code added

---
+++ Stop Watch/src/Stopwatch.jsx
@@ -0,0 +1,75 @@
+import React, { useState,useRef, useEffect } from 'react'
+
+const Stopwatch = () => {
+
+    const [time,setTime]=useState(0)
+    const stopwatchRef=useRef(0)
+    const intervalRef=useRef(null)
+    const needToResumeRef=useRef(false)
+    function handleStart()
+    {
+        stopwatchRef.current=new Date().getTime() - time;
+        intervalRef.current=setInterval(()=>{
+            setTime(new Date().getTime()-stopwatchRef.current)
+        },10)
+
+    }
+    function handlePause()
+    {
+        clearInterval(intervalRef.current)
+        intervalRef.current=null
+    }
+
+    function handleReset()
+    {
+        clearInterval(intervalRef.current)
+        intervalRef.current=null
+        needToResumeRef.current=false
+        stopwatchRef.current=0
+        setTime(0)
+    }
+
+    useEffect(()=>{
+        window.addEventListener("blur",handleBlur)
+        window.addEventListener("focus",handleFocus)
+        return ()=>{
+            window.removeEventListener("blur",handleBlur)
+            window.removeEventListener("focus",handleFocus)
+        }
+    },[time])
+
+    function handleBlur()
+    {
+        needToResumeRef.current=!!intervalRef.current;
+        clearInterval(intervalRef.current)
+    }
+
+    function handleFocus()
+    {
+        if(needToResumeRef.current)
+        {
+            needToResumeRef.current=false
+            handleStart()
+        }
+    }
+
+    function formatTime(){
+        const ms=Math.floor((time%1000)/10).toString().padStart(2,"0")
+        const sec=Math.floor((time/1000)%60).toString().padStart(2,"0")
+        const min=Math.floor((time/(1000*60))%60).toString().padStart(2,"0")
+        const hr=Math.floor((time/(1000*60*60))).toString().padStart(2,"0")
+        return `${hr}:${min}:${sec}:${ms}`
+    }
+    return (
+        <div className='stopwatch'>
+        <span className='time'>{formatTime()}</span>
+        <div>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handlePause}>Pause</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+        </div>
+    )
+}
+
+export default Stopwatch
