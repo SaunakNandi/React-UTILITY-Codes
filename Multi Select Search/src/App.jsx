ad413ce897067a6eb37a65f05a9b02bd8470f7f3
SaunakNandi <saunaknandi021000@gmail.com>
2025-07-20 21:06:14 +0530

    Code modificaiton for multi select

--- Multi Select Search/src/App.jsx
+++ Multi Select Search/src/App.jsx
@@ -5,11 +5,15 @@ import Pills from './Pills'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
+  const [userData,setUserData]=useState([])
+  const [skipData,setSkipData]=useState(0)
+  const [loading,setLoading]=useState(true)
   const [activeSuggestion, setactiveSuggestion]=useState(0)
   const [suggestions, setSuggestions] = useState([])
   const [selectedUsers, setSelectedUsers] = useState([])
   const [selectedUsersSet, setSelectedUsersSet] = useState(new Set())
   const inputRef=useRef(null)
+  const listItemRef=useRef([])
 
   const handleSelectUser=(user)=>{
     setSelectedUsers([...selectedUsers,user])
@@ -17,6 +21,13 @@ function App() {
     setSearchTerm("")
     setSuggestions([])
     inputRef.current.focus()
+  }
+  function scrollToItem(index)
+  {
+    listItemRef.current[index]?.scrollIntoView({
+      behavior:'smooth',
+      block:'nearest'
+    })
   }
   function handleKeyDown(e)
   {
@@ -27,24 +38,29 @@ function App() {
       handleRemoveUser(lastUser)
       setSuggestions([])
     }
-    if(e.key==='ArrowDown' && suggestions?.users?.length>0)
+    if(e.key==='ArrowDown' && suggestions?.length>0)
     {
-      if(activeSuggestion<suggestions.users?.length-1)
-        setactiveSuggestion(activeSuggestion+1)
-      else 
-        setactiveSuggestion(0)
+      e.preventDefault()
+      const newIdx=activeSuggestion<suggestions.length-1?activeSuggestion+1:0;
+      setactiveSuggestion(newIdx)
+      scrollToItem(newIdx)  
     }
-    else if(e.key==='ArrowUp' && suggestions?.users?.length>0)
+    else if(e.key==='ArrowUp' && suggestions?.length>0)
     {
-       e.preventDefault()
-      if(activeSuggestion>0)
-        setactiveSuggestion(activeSuggestion-1)
-      else
-        setactiveSuggestion(suggestions.users.length-1)
+      e.preventDefault()
+      const newIdx=activeSuggestion>0?activeSuggestion-1:activeSuggestion?.length-1
+      setactiveSuggestion(newIdx)
+      scrollToItem(newIdx)
     }
-    else if(e.key==='Enter' && suggestions?.users?.length>0)
+    else if(e.key==='Enter' && suggestions?.length>0)
     {
-      handleSelectUser(suggestions.users[activeSuggestion])
+      handleSelectUser(suggestions[activeSuggestion])
+    }
+    else
+    {
+      const filtered=userData.filter((x)=>x.name.includes(e.target.value))
+      console.log("filtered ",filtered)
+      setSuggestions(filtered)
     }
   }
   const handleRemoveUser=(user)=>{
@@ -54,23 +70,27 @@ function App() {
     updatedEmails.delete(user.email)
     setSelectedUsersSet(updatedEmails)
   }
-  const fetchUsers=()=>{
-    if(searchTerm.trim()==="")
-    {
-      setSuggestions([])
-      return
-    }
-    fetch(`https://dummyjson.com/users/search?q=${searchTerm}`)
-    .then((res)=>res.json())
-    .then((data)=>setSuggestions(data))
-    .catch((err)=>console.error(err))
+  const fetchUsers=async()=>{
+    const res=await fetch(`https://dummyjson.com/users?skip=${skipData}`)
+    const data=await res.json()
+    const {limit,skip,total}=data
+    const modifiedData=data.users.map((x)=>{
+      return {
+        ...x,
+        name:x.firstName+ " "+x.lastName
+      }
+    })
+    setUserData(modifiedData)
+    if(skip+limit<total)
+      setSkipData(skip+limit)
+    else
+      setLoading(false)
   }
   useEffect(()=>{
     fetchUsers()
-    console.log(suggestions)
-  },[searchTerm])
+  },[skipData])
   
-  return (
+  return !loading && (
     <>
       <div className='user-search-container'>
         <div className="user-search-input">
@@ -89,11 +109,12 @@ function App() {
             {/* Search Suggestions */}
             <ul className='suggestions-list'>
               {
-                suggestions?.users?.map((user,i)=>{
-                  // console.log(i,activeSuggestion)
+                suggestions.length>0 && suggestions.map((user,i)=>{
+                  // hiding selected users from suggestion list
                   return !selectedUsersSet.has(user.email)?(
                   <li onClick={()=>handleSelectUser(user)} key={user.id}
-                  className={activeSuggestion==i? 'active':''}>
+                  className={activeSuggestion==i? 'active':''}
+                  ref={(el)=>listItemRef.current[i]=el}>
                     <img src={user.image} alt={`${user.firstName} ${user.lastName}`}/>
                     <span>{user.firstName} {user.lastName}</span>
                   </li>
