eecad6c54df0aede153337961bd6bd5c92217a29
SaunakNandi <saunaknandi021000@gmail.com>
2025-12-28 13:22:10 +0530

    Pagination updated for unstable window length

--- Pagination AMAZON/src/App.jsx
+++ Pagination AMAZON/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Pagination from './Pagination'
+import { useState } from "react";
+import "./App.css";
+import Pagination from "./Pagination";
 
 function App() {
-  const [currentPage,setCurrentPage]=useState(1)
-  function onPageChanges(page)
-  {
-    setCurrentPage(page)
+  const [currentPage, setCurrentPage] = useState(1);
+  function onPageChanges(page) {
+    setCurrentPage(page);
   }
   return (
     <>
-      <Pagination visibleSlots={6} currentPage={currentPage} start={1} end={20} onPageChange={onPageChanges}/>
+      <Pagination
+        visibleSlots={6}
+        currentPage={currentPage}
+        start={1}
+        end={20}
+        onPageChange={onPageChanges}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Pagination AMAZON/src/Pagination.jsx
+++ Pagination AMAZON/src/Pagination.jsx
@@ -1,56 +1,74 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from "react";
 
-const Pagination = ({visibleSlots,currentPage=1,start=1,end,onPageChange}) => {
-    const buttonRef=useRef([])
-    function generatePagination(start,end,visibleSlots,currentPage)
-    {
-        let result=[]
-        if(start+visibleSlots==end-1)
-        {
-            for(let i=start;i<=end;i++)
-                result.push(i)
-            return result
-        }
-        let middle=Math.floor(visibleSlots/2)
-        let leftPointer=Math.max((currentPage-middle),start+1)
-        let rightPointer=leftPointer+visibleSlots-1
-        if(rightPointer>=end)
-            rightPointer=end-1
-        result=[start]
-        if(leftPointer>start+1)
-            result.push("...")
-        if(rightPointer>=end-1) 
-        {
-            for(let i=leftPointer-(visibleSlots-(rightPointer-leftPointer+1));i<=rightPointer;i++)
-                result.push(i)
-        }
-        else
-        {
-            for(let i=leftPointer;i<=rightPointer-1;i++)
-                result.push(i)
-            result.push("...")
-        }
-        result.push(end)
-        // buttonRef.current[currentPage-1]?.focus()
-        console.log("result ",result,currentPage)
-        return result
+const Pagination = ({
+  visibleSlots,
+  currentPage = 1,
+  start = 1,
+  end,
+  onPageChange,
+}) => {
+  const buttonRef = useRef([]);
+  function generatePagination(start, end, visibleSlots, currentPage) {
+    let result = [];
+    if (start + visibleSlots == end - 1) {
+      for (let i = start; i <= end; i++) result.push(i);
+      return result;
     }
-    const pagination=generatePagination(start,end,visibleSlots-1,currentPage)
+    let middle = Math.floor(visibleSlots / 2);
+    let leftPointer = Math.max(currentPage - middle, start + 1);
+    let rightPointer = leftPointer + visibleSlots - 1;
+    if (rightPointer >= end) rightPointer = end - 1;
+    result = [start];
+    if (leftPointer > start + 1) {
+      result.push("...");
+      // since "..." aleadry added visible slots is reduced and leftPointer is shifted 1 step right
+      --visibleSlots;
+      ++leftPointer;
+    }
+    if (rightPointer >= end - 1) {
+      for (
+        let i = leftPointer - (visibleSlots - (rightPointer - leftPointer + 1));
+        i <= rightPointer;
+        i++
+      )
+        result.push(i);
+    } else {
+      for (let i = leftPointer; i <= rightPointer - 1; i++) result.push(i);
+      result.push("...");
+    }
+    result.push(end);
+    return result;
+  }
+  const pagination = generatePagination(
+    start,
+    end,
+    visibleSlots - 1,
+    currentPage
+  );
   return (
-    <div className='pagination'>
-        <button onClick={()=>currentPage>start && onPageChange(currentPage-1)}>⬅️</button>
-        {
-            pagination.map((page,i)=>(
-                <button className="page_no" onClick={()=>onPageChange(page)}
-                ref={(el)=>buttonRef.current[page]=el}
-                style={{border:`${currentPage==page?'2px solid green':''}`}}>
-                    {page}
-                </button>
-            ))
-        }
-        <button onClick={()=>currentPage<end && onPageChange(currentPage+1)}>➡️</button>
+    <div className="pagination">
+      <button
+        onClick={() => currentPage > start && onPageChange(currentPage - 1)}
+      >
+        ⬅️
+      </button>
+      {pagination.map((page) => (
+        <button
+          className="page_no"
+          onClick={() => onPageChange(page)}
+          ref={(el) => (buttonRef.current[page] = el)}
+          style={{ border: `${currentPage == page ? "2px solid green" : ""}` }}
+        >
+          {page}
+        </button>
+      ))}
+      <button
+        onClick={() => currentPage < end && onPageChange(currentPage + 1)}
+      >
+        ➡️
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
