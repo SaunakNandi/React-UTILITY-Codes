866f3cf738f024118bd78afbd401fb81b6491027
SaunakNandi <saunaknandi021000@gmail.com>
2026-06-06 21:55:00 +0530

    new throttle code

---
+++ useThrottle/src/use-scroll.jsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useRef } from "react";
+
+const useScroll = (callback, delay) => {
+  const timeoutRef = useRef(null);
+  const lastUpdatedTimeRef = useRef(0);
+  const callbackRef = useRef(null);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return useCallback(() => {
+    const now = new Date();
+    const timeRemaining = delay - (now - lastUpdatedTimeRef.current);
+
+    if (timeRemaining <= 0) {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
+      }
+      callbackRef.current(...args);
+      lastUpdatedTimeRef.current = now;
+    } else if (!timeoutRef.current) {
+      timeoutRef.current = setTimeout(() => {
+        lastUpdatedTimeRef.current = new Date();
+        callbackRef.current(...args);
+        timeoutRef.current = null;
+      }, timeRemaining);
+    }
+  }, [delay]);
+};
